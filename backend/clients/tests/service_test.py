e4b46a81db1ab9c451013be80fabfe20c1a8a0d2
Katyusha <katyusha055@proton.me>
2026-04-25 20:34:58 -0500

    Created fixtures for testing the clients feature in the API

---
+++ backend/clients/tests/service_test.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import pytest
+from dotenv import load_dotenv
+from fastapi.testclient import TestClient
+
+tests_dir = Path(__file__).resolve().parent
+test_env_file = tests_dir / '.env.tests'
+
+@pytest.fixture(scope='session', autouse=True)
+def load_test_env():
+    load_dotenv(dotenv_path=test_env_file, override=True)
+    
+@pytest.fixture()
+def api_client():
+    from backend.main import app
+    with TestClient(app) as client:
+        yield client
+
+@pytest.fixture(autouse=True)
+def clean_db():
+    yield
+    from backend.database.connect import connect
+    with connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute("TRUNCATE TABLE clients, users RESTART IDENTITY CASCADE;")
+
+@pytest.fixture()
+def create_user():
+    from backend.database.connect import connect
+    with connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                INSERT INTO users (name, phone, password_hash)
+                VALUES ('Test User', '1234567890', 'hashed_password')
+                RETURNING id;
+            """)
+            user_id = cur.fetchone()[0]
+    return user_id
