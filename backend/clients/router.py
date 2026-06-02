1a5e94385eae47b900dd2bd639bb86292e8950d2
Katyusha <katyusha055@proton.me>
2026-06-01 23:24:44 -0500

    Implemented real auth system into clients and tickets feature, finally getting rid of hardcoded "auth"

--- backend/clients/router.py
+++ backend/clients/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 import backend.clients.models as mdl
 import backend.clients.service as ser
-from backend.core.auth import get_user_id
+from backend.auth.utils import CurrentUser
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,38 +12,38 @@ router = APIRouter(
 )
 
 @router.get('/', response_model=list[mdl.ResponseClient])
-def get_clients_endpoint():
-    current_user_id = {'user_id': get_user_id()} #this is to test the program, later i'll replace it with a token and auth system or something like that
+def get_clients_endpoint(user: CurrentUser):
+    current_user_id = {'user_id': user.id} 
     return ser.get_clients_service(current_user_id)
 
 
 @router.post('/', response_model=mdl.ResponseClient)
-def create_client_endpoint(payload: mdl.CreateClient):
+def create_client_endpoint(payload: mdl.CreateClient, user: CurrentUser):
     client_data = {
         **payload.model_dump(),
-        "user_id": get_user_id()
+        "user_id": user.id
     }
     return ser.create_client_service(client_data)
 
 @router.patch('/{client_id}', response_model=mdl.ResponseClient)
-def update_client_endpoint(client_id: int, payload: mdl.UpdateClient):
-    current_user_id = get_user_id()  # mock auth for testing purposes
+def update_client_endpoint(client_id: int, payload: mdl.UpdateClient, user: CurrentUser):
+    current_user_id = user.id
     update_data = payload.model_dump(exclude_none=True)
     return ser.update_client_service(current_user_id, client_id, update_data)
 
 
 @router.delete('/{client_id}')
-def delete_client_endpoint(client_id: int):
-    current_user_id = get_user_id()  # mock auth for testing purposes
+def delete_client_endpoint(client_id: int, user: CurrentUser):
+    current_user_id = user.id
     return ser.delete_client_service(current_user_id, client_id)
 
 
 @router.get('/{client_id}', response_model=mdl.ResponseClient)
-def get_client_by_id_endpoint(client_id: int):
-    current_user_id = get_user_id()  # mock auth for testing purposes
+def get_client_by_id_endpoint(client_id: int, user: CurrentUser):
+    current_user_id = user.id
     return ser.get_client_by_id_service(current_user_id, client_id)
 
 @router.get('/by-phone/{phone}', response_model=mdl.ResponseClient)
-def get_client_by_phone_endpoint(phone: str):
-    current_user_id = get_user_id()  # mock auth for testing purposes
+def get_client_by_phone_endpoint(phone: str, user: CurrentUser):
+    current_user_id = user.id
     return ser.get_client_by_phone_service(current_user_id, phone)
--- backend/core/auth.py
+++
@@ -1,2 +0,0 @@
-def get_user_id():
-    return 1 #for testing purposes it will stay like this until i do the auth system
