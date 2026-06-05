6d4ac9a7e62191d3bed55a7057e3f8474c0b9a75
Katyusha <katyusha055@proton.me>
2026-06-05 17:38:34 -0500

    Implemented auth into all api endpoints replacing the mock auth

--- backend/clients/router.py
+++ backend/clients/router.py
@@ -13,7 +13,7 @@ router = APIRouter(
 
 @router.get('/', response_model=list[mdl.ResponseClient])
 def get_clients_endpoint(user: CurrentUser):
-    current_user_id = {'user_id': user.id} 
+    current_user_id = {'user_id': user['id']} 
     return ser.get_clients_service(current_user_id)
 
 
@@ -21,29 +21,29 @@ def get_clients_endpoint(user: CurrentUser):
 def create_client_endpoint(payload: mdl.CreateClient, user: CurrentUser):
     client_data = {
         **payload.model_dump(),
-        "user_id": user.id
+        "user_id": user['id']
     }
     return ser.create_client_service(client_data)
 
 @router.patch('/{client_id}', response_model=mdl.ResponseClient)
 def update_client_endpoint(client_id: int, payload: mdl.UpdateClient, user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     update_data = payload.model_dump(exclude_none=True)
     return ser.update_client_service(current_user_id, client_id, update_data)
 
 
 @router.delete('/{client_id}')
 def delete_client_endpoint(client_id: int, user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     return ser.delete_client_service(current_user_id, client_id)
 
 
 @router.get('/{client_id}', response_model=mdl.ResponseClient)
 def get_client_by_id_endpoint(client_id: int, user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     return ser.get_client_by_id_service(current_user_id, client_id)
 
 @router.get('/by-phone/{phone}', response_model=mdl.ResponseClient)
 def get_client_by_phone_endpoint(phone: str, user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     return ser.get_client_by_phone_service(current_user_id, phone)
--- backend/tickets/router.py
+++ backend/tickets/router.py
@@ -11,36 +11,36 @@ router = APIRouter(
 
 @router.post('/', response_model=mdl.TicketResponse)
 def create_ticket_endpoint(payload: mdl.TicketCreate, user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     return ser.create_ticket(current_user_id, payload.model_dump())
 
 
 @router.get('/', response_model=list[mdl.TicketResponse])
 def get_tickets_endpoint(user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     return ser.get_tickets(current_user_id)
 
 
 @router.get('/{ticket_id}', response_model=mdl.TicketResponse)
 def get_ticket_by_id_endpoint(ticket_id: int, user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     return ser.get_ticket_by_id(current_user_id, ticket_id)
 
 
 @router.delete('/{ticket_id}', status_code=204)
 def delete_ticket_endpoint(ticket_id: int, user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     ser.delete_ticket(current_user_id, ticket_id)
     return Response(status_code=204)
 
 
 @router.patch('/{ticket_id}/status', response_model=mdl.TicketResponse)
 def update_ticket_status_endpoint(ticket_id: int, payload: mdl.TicketStatusUpdate, user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     return ser.update_ticket_status(current_user_id, ticket_id, payload.status)
 
 
 @router.patch('/{ticket_id}', response_model=mdl.TicketResponse)
 def update_ticket_endpoint(ticket_id: int, payload: mdl.TicketUpdate, user: CurrentUser):
-    current_user_id = user.id
+    current_user_id = user['id']
     return ser.update_ticket(current_user_id, ticket_id, payload.model_dump())
