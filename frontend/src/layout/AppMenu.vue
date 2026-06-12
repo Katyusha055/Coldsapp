0cfe42d3ba8423d7dea537c13a891359433835c8
Katyusha <katyusha055@proton.me>
2026-06-11 21:33:34 -0500

    Template cleanup to match project necessities

--- frontend/src/layout/AppMenu.vue
+++ frontend/src/layout/AppMenu.vue
@@ -38,91 +38,91 @@ const model = ref([
                 icon: 'pi pi-fw pi-table',
                 to: '/uikit/table'
             },
-            {
-                label: 'List',
-                icon: 'pi pi-fw pi-list',
-                to: '/uikit/list'
-            },
-            {
-                label: 'Tree',
-                icon: 'pi pi-fw pi-share-alt',
-                to: '/uikit/tree'
-            },
-            {
-                label: 'Panel',
-                icon: 'pi pi-fw pi-tablet',
-                to: '/uikit/panel'
-            },
+            // {
+            //     label: 'List',
+            //     icon: 'pi pi-fw pi-list',
+            //     to: '/uikit/list'
+            // },
+            // {
+            //     label: 'Tree',
+            //     icon: 'pi pi-fw pi-share-alt',
+            //     to: '/uikit/tree'
+            // },
+            // {
+            //     label: 'Panel',
+            //     icon: 'pi pi-fw pi-tablet',
+            //     to: '/uikit/panel'
+            // },
             {
                 label: 'Overlay',
                 icon: 'pi pi-fw pi-clone',
                 to: '/uikit/overlay'
             },
-            {
-                label: 'Media',
-                icon: 'pi pi-fw pi-image',
-                to: '/uikit/media'
-            },
-            {
-                label: 'Menu',
-                icon: 'pi pi-fw pi-bars',
-                to: '/uikit/menu'
-            },
+            // {
+            //     label: 'Media',
+            //     icon: 'pi pi-fw pi-image',
+            //     to: '/uikit/media'
+            // },
+            // {
+            //     label: 'Menu',
+            //     icon: 'pi pi-fw pi-bars',
+            //     to: '/uikit/menu'
+            // },
             {
                 label: 'Message',
                 icon: 'pi pi-fw pi-comment',
                 to: '/uikit/message'
             },
-            {
-                label: 'File',
-                icon: 'pi pi-fw pi-file',
-                to: '/uikit/file'
-            },
-            {
-                label: 'Chart',
-                icon: 'pi pi-fw pi-chart-bar',
-                to: '/uikit/charts'
-            },
-            {
-                label: 'Timeline',
-                icon: 'pi pi-fw pi-calendar',
-                to: '/uikit/timeline'
-            },
-            {
-                label: 'Misc',
-                icon: 'pi pi-fw pi-circle',
-                to: '/uikit/misc'
-            }
+            // {
+            //     label: 'File',
+            //     icon: 'pi pi-fw pi-file',
+            //     to: '/uikit/file'
+            // },
+            // {
+            //     label: 'Chart',
+            //     icon: 'pi pi-fw pi-chart-bar',
+            //     to: '/uikit/charts'
+            // },
+            // {
+            //     label: 'Timeline',
+            //     icon: 'pi pi-fw pi-calendar',
+            //     to: '/uikit/timeline'
+            // },
+            // {
+            //     label: 'Misc',
+            //     icon: 'pi pi-fw pi-circle',
+            //     to: '/uikit/misc'
+            // }
         ]
     },
-    {
-        label: 'Prime Blocks',
-        icon: 'pi pi-fw pi-prime',
-        path: '/blocks',
-        items: [
-            {
-                label: 'Free Blocks',
-                icon: 'pi pi-fw pi-eye',
-                to: '/blocks/free'
-            },
-            {
-                label: 'All Blocks',
-                icon: 'pi pi-fw pi-globe',
-                url: 'https://blocks.primevue.org/',
-                target: '_blank'
-            }
-        ]
-    },
+    // {
+    //     label: 'Prime Blocks',
+    //     icon: 'pi pi-fw pi-prime',
+    //     path: '/blocks',
+    //     items: [
+    //         {
+    //             label: 'Free Blocks',
+    //             icon: 'pi pi-fw pi-eye',
+    //             to: '/blocks/free'
+    //         },
+    //         {
+    //             label: 'All Blocks',
+    //             icon: 'pi pi-fw pi-globe',
+    //             url: 'https://blocks.primevue.org/',
+    //             target: '_blank'
+    //         }
+    //     ]
+    // },
     {
         label: 'Pages',
         icon: 'pi pi-fw pi-briefcase',
         path: '/pages',
         items: [
-            {
-                label: 'Landing',
-                icon: 'pi pi-fw pi-globe',
-                to: '/landing'
-            },
+            // {
+            //     label: 'Landing',
+            //     icon: 'pi pi-fw pi-globe',
+            //     to: '/landing'
+            // },
             {
                 label: 'Auth',
                 icon: 'pi pi-fw pi-user',
@@ -239,23 +239,23 @@ const model = ref([
             }
         ]
     },
-    {
-        label: 'Get Started',
-        path: '/start',
-        items: [
-            {
-                label: 'Documentation',
-                icon: 'pi pi-fw pi-book',
-                to: '/start/documentation'
-            },
-            {
-                label: 'View Source',
-                icon: 'pi pi-fw pi-github',
-                url: 'https://github.com/primefaces/sakai-vue',
-                target: '_blank'
-            }
-        ]
-    }
+    // {
+    //     label: 'Get Started',
+    //     path: '/start',
+    //     items: [
+    //         {
+    //             label: 'Documentation',
+    //             icon: 'pi pi-fw pi-book',
+    //             to: '/start/documentation'
+    //         },
+    //         {
+    //             label: 'View Source',
+    //             icon: 'pi pi-fw pi-github',
+    //             url: 'https://github.com/primefaces/sakai-vue',
+    //             target: '_blank'
+    //         }
+    //     ]
+    // }
 ]);
 </script>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -33,70 +33,70 @@ const router = createRouter({
                     name: 'table',
                     component: () => import('@/views/uikit/TableDoc.vue')
                 },
-                {
-                    path: '/uikit/list',
-                    name: 'list',
-                    component: () => import('@/views/uikit/ListDoc.vue')
-                },
-                {
-                    path: '/uikit/tree',
-                    name: 'tree',
-                    component: () => import('@/views/uikit/TreeDoc.vue')
-                },
-                {
-                    path: '/uikit/panel',
-                    name: 'panel',
-                    component: () => import('@/views/uikit/PanelsDoc.vue')
-                },
+                // {
+                //     path: '/uikit/list',
+                //     name: 'list',
+                //     component: () => import('@/views/uikit/ListDoc.vue')
+                // },
+                // {
+                //     path: '/uikit/tree',
+                //     name: 'tree',
+                //     component: () => import('@/views/uikit/TreeDoc.vue')
+                // },
+                // {
+                //     path: '/uikit/panel',
+                //     name: 'panel',
+                //     component: () => import('@/views/uikit/PanelsDoc.vue')
+                // },
 
                 {
                     path: '/uikit/overlay',
                     name: 'overlay',
                     component: () => import('@/views/uikit/OverlayDoc.vue')
                 },
-                {
-                    path: '/uikit/media',
-                    name: 'media',
-                    component: () => import('@/views/uikit/MediaDoc.vue')
-                },
+                // {
+                //     path: '/uikit/media',
+                //     name: 'media',
+                //     component: () => import('@/views/uikit/MediaDoc.vue')
+                // },
                 {
                     path: '/uikit/message',
                     name: 'message',
                     component: () => import('@/views/uikit/MessagesDoc.vue')
                 },
-                {
-                    path: '/uikit/file',
-                    name: 'file',
-                    component: () => import('@/views/uikit/FileDoc.vue')
-                },
-                {
-                    path: '/uikit/menu',
-                    name: 'menu',
-                    component: () => import('@/views/uikit/MenuDoc.vue')
-                },
-                {
-                    path: '/uikit/charts',
-                    name: 'charts',
-                    component: () => import('@/views/uikit/ChartDoc.vue')
-                },
-                {
-                    path: '/uikit/misc',
-                    name: 'misc',
-                    component: () => import('@/views/uikit/MiscDoc.vue')
-                },
-                {
-                    path: '/uikit/timeline',
-                    name: 'timeline',
-                    component: () => import('@/views/uikit/TimelineDoc.vue')
-                },
-                {
+                // {
+                //     path: '/uikit/file',
+                //     name: 'file',
+                //     component: () => import('@/views/uikit/FileDoc.vue')
+                // },
+                // {
+                //     path: '/uikit/menu',
+                //     name: 'menu',
+                //     component: () => import('@/views/uikit/MenuDoc.vue')
+                // },
+                // {
+                //     path: '/uikit/charts',
+                //     name: 'charts',
+                //     component: () => import('@/views/uikit/ChartDoc.vue')
+                // },
+                // {
+                //     path: '/uikit/misc',
+                //     name: 'misc',
+                //     component: () => import('@/views/uikit/MiscDoc.vue')
+                // },
+                // {
+                //     path: '/uikit/timeline',
+                //     name: 'timeline',
+                //     component: () => import('@/views/uikit/TimelineDoc.vue')
+                // },
+                /* Prime Blocks{
                     path: '/blocks/free',
                     name: 'blocks',
                     meta: {
                         breadcrumb: ['Prime Blocks', 'Free Blocks']
                     },
                     component: () => import('@/views/utilities/Blocks.vue')
-                },
+                },*/
                 {
                     path: '/pages/empty',
                     name: 'empty',
@@ -107,18 +107,18 @@ const router = createRouter({
                     name: 'crud',
                     component: () => import('@/views/pages/Crud.vue')
                 },
-                {
-                    path: '/start/documentation',
-                    name: 'documentation',
-                    component: () => import('@/views/pages/Documentation.vue')
-                }
+                // {
+                //     path: '/start/documentation',
+                //     name: 'documentation',
+                //     component: () => import('@/views/pages/Documentation.vue')
+                // }
             ]
         },
-        {
-            path: '/landing',
-            name: 'landing',
-            component: () => import('@/views/pages/Landing.vue')
-        },
+        // {
+        //     path: '/landing',
+        //     name: 'landing',
+        //     component: () => import('@/views/pages/Landing.vue')
+        // },
         {
             path: '/pages/notfound',
             name: 'notfound',
