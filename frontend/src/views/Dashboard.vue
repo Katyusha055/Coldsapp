8993178a6bbadba93a9f3414f48ca2db07801c14
Katyusha <katyusha055@proton.me>
2026-06-16 17:06:27 -0500

    refactor: rewire Dashboard to use Coldsapp widgets

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,22 +1,19 @@
 <script setup>
-import BestSellingWidget from '@/components/dashboard/BestSellingWidget.vue';
-import NotificationsWidget from '@/components/dashboard/NotificationsWidget.vue';
-import RecentSalesWidget from '@/components/dashboard/RecentSalesWidget.vue';
-import RevenueStreamWidget from '@/components/dashboard/RevenueStreamWidget.vue';
-import StatsWidget from '@/components/dashboard/StatsWidget.vue';
+import ContactWidget from '@/components/dashboard/ContactWidget.vue';
+import TopClientsWidget from '@/components/dashboard/TopClientsWidget.vue';
+import PendingTicketsWidget from '@/components/dashboard/PendingTicketsWidget.vue';
 </script>
 
 <template>
     <div class="grid grid-cols-12 gap-8">
-        <StatsWidget />
-
-        <div class="col-span-12 xl:col-span-6">
-            <RecentSalesWidget />
-            <BestSellingWidget />
+        <div class="col-span-12">
+            <ContactWidget />
         </div>
         <div class="col-span-12 xl:col-span-6">
-            <RevenueStreamWidget />
-            <NotificationsWidget />
+            <TopClientsWidget />
+        </div>
+        <div class="col-span-12 xl:col-span-6">
+            <PendingTicketsWidget />
         </div>
     </div>
 </template>
